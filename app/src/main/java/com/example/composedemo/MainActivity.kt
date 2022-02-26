997c318cb9099980f79f7a0dbb9dede1fc270c6f
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2022-02-26 23:31:57 +0300

    Some test algebra examples

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,7 +16,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedemo.ComponentExamples.checkLinearLayoutHorizontal
@@ -28,19 +32,47 @@ class MainActivity : ComponentActivity() {
             ComposeDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    checkLinearLayoutHorizontal()
+                    initial()
                 }
             }
         }
     }
 }
 
+@Composable
+fun initial() {
 
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(
+            modifier = Modifier.offset(y = 16.dp),
+            text = "Кольца, модули алгебры, категории",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = "Кольцо R - это абелева группа (Т.е. включает операцию сложения " +
+                    "+ и нейтральный элемент 0), в которой так же определен вторая операция - умножение",
+            modifier = Modifier.offset(y = 16.dp, x = 8.dp).padding(end = 8.dp)
+        )
+
+        Text(
+            text = "Кольцо ассоциативно, если операция умножения ассоциативна a(bc) = (ab)c",
+            modifier = Modifier.offset(y = 16.dp, x = 8.dp).padding(end = 8.dp)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeDemoTheme {
-        checkLinearLayoutHorizontal()
+        initial()
     }
 }
