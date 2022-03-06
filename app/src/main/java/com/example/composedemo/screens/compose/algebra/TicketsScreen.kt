7e02cfbae53d6a2f50eed197b501b8dd7539e4f3
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2022-03-07 01:31:46 +0300

    Scrolling row with ticket filters

--- app/src/main/java/com/example/composedemo/screens/compose/algebra/TicketsScreen.kt
+++ app/src/main/java/com/example/composedemo/screens/compose/algebra/TicketsScreen.kt
@@ -1,9 +1,13 @@
 package com.example.composedemo.screens.compose.algebra
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.composedemo.R
 
@@ -11,14 +15,31 @@ import com.example.composedemo.R
  * @author Yana Glad
  */
 @Composable
-fun TicketsScreen (navController: NavController) {
-    Row {
-        // TODO кнопки фильтрации по номерам задач
+fun TicketsScreen(navController: NavController) {
+    Row(modifier = Modifier
+        .padding(top = 8.dp)
+        .horizontalScroll(rememberScrollState())
+    ) {
+        SimpleButton("Список билетов")
+        SimpleButton("Номер 1")
+        SimpleButton("Номер 2")
+        SimpleButton("Номер 3")
+        SimpleButton("Номер 4")
     }
 
     Column {
         val images = listOf(R.drawable.first_algebra)
         // TODO адрес картинок
         // Через for each. Создать сущность проверки номера
+    }
+}
+
+@Composable
+private fun SimpleButton(text: String) {
+    Button(
+        modifier = Modifier.padding(start = 8.dp),
+        onClick = { }
+    ) {
+        Text(text = text)
     }
 }
