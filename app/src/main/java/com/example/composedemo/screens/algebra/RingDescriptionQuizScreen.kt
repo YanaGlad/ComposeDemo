d59b3fb43cd57fb888ce446ed369335194cceba7
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2022-03-06 11:51:01 +0300

    Quiz checkbox

--- app/src/main/java/com/example/composedemo/screens/algebra/RingDescriptionQuizScreen.kt
+++ app/src/main/java/com/example/composedemo/screens/algebra/RingDescriptionQuizScreen.kt
@@ -1,18 +1,23 @@
 package com.example.composedemo.screens.algebra
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Checkbox
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composedemo.screens.utils.CheckBoxWithText
+import com.example.composedemo.R
+import com.example.composedemo.screens.utils.CheckBoxUtils.CheckBoxWithText
+import com.example.composedemo.screens.utils.CheckBoxUtils.QuizCheckBox
 
 /*
  * @author Yana Glad
@@ -32,20 +37,23 @@ fun RingDescribeScreenQuiz() {
             textAlign = TextAlign.Center
         )
 
-        Text(
-            text = "1) Что включает в себя понятие кольца?",
-            modifier = Modifier
-                .offset(x = 16.dp)
-                .padding(top = 20.dp),
-            fontSize = 16.sp,
+        QuizCheckBox(
+            quizTitle = "1) Что включает в себя понятие кольца?",
+            countCheckBox = 5,
+            listOfNames = listOf("Операция сложения +", "Нейтральный элемент",
+                "Операция вычитания -", "Обратимый элемент", "Операция умножения *"),
+            correctIndexes = listOf(0, 1, 4),
+            onSuccess = { showSuccess() },
+            onFail = { showFail() }
         )
-
-        CheckBoxWithText("Операция сложения +", 16, 8)
-        CheckBoxWithText("Нейтральный элемент", 16, 8)
-        CheckBoxWithText("Операция вычитания -", 16, 8)
-        CheckBoxWithText("Обратимый элемент", 16, 8)
-        CheckBoxWithText("Операция умножения *", 16, 8)
-
     }
+}
+
+fun showFail() {
+    println("NO")
+}
+
+fun showSuccess() {
+    println("YES")
 }
 
--- app/src/main/java/com/example/composedemo/screens/utils/Utils.kt
+++ app/src/main/java/com/example/composedemo/screens/utils/Utils.kt
@@ -1,36 +1,98 @@
 package com.example.composedemo.screens.utils
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.composedemo.R
 
+/*
+ * @author Yana Glad
+ */
+object CheckBoxUtils {
 
-@Composable
-fun CheckBoxWithText(text: String, fontSize: Int, padding: Int) {
-    val checkedState = remember { mutableStateOf(false) }
+    @Composable
+    fun CheckBoxWithText(text: String, fontSize: Int, padding: Int, checkedState: MutableState<Boolean>) {
+        Row {
+            Checkbox(
+                modifier = Modifier
+                    .offset(x = 16.dp)
+                    .padding(top = 8.dp),
+                checked = checkedState.value,
+                onCheckedChange = { checkedState.value = it }
+            )
+            Text(
+                text = text,
+                fontSize = fontSize.sp,
+                modifier = Modifier
+                    .offset(x = 18.dp)
+                    .padding(top = padding.dp),
+            )
+        }
+    }
 
-    Row {
-        Checkbox(
+    @Composable
+    fun QuizCheckBox(
+        quizTitle: String, countCheckBox: Int, listOfNames: List<String>,
+        correctIndexes: List<Int>, onSuccess: () -> Unit, onFail: () -> Unit,
+    ) {
+
+        Text(
+            text = quizTitle,
             modifier = Modifier
                 .offset(x = 16.dp)
-                .padding(top = 8.dp),
-            checked = checkedState.value,
-            onCheckedChange = { checkedState.value = it }
+                .padding(top = 20.dp),
+            fontSize = 16.sp,
         )
-        Text(
-            text = text,
-            fontSize = fontSize.sp,
+
+        val checkStates = arrayListOf<MutableState<Boolean>>()
+
+        for (i in 0..countCheckBox) {
+            checkStates.add(remember { mutableStateOf(false) })
+        }
+
+        CheckBoxWithText(listOfNames[0], 16, 8, checkStates[0])
+        CheckBoxWithText(listOfNames[1], 16, 8, checkStates[1])
+        CheckBoxWithText(listOfNames[2], 16, 8, checkStates[2])
+        CheckBoxWithText(listOfNames[3], 16, 8, checkStates[3])
+        CheckBoxWithText(listOfNames[4], 16, 8, checkStates[4])
+
+        Button(
+            onClick = {
+                var check = true
+                for (index in 0 until countCheckBox) {
+                    if (index !in correctIndexes && checkStates[index].value) check = false
+                    if (index in correctIndexes && !checkStates[index].value) check = false
+                }
+                if (check) {
+                    onSuccess()
+                } else {
+                    onFail()
+                }
+            },
             modifier = Modifier
-                .offset(x = 18.dp)
-                .padding(top = padding.dp),
-        )
+                .padding(top = 50.dp)
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.teal_700), contentColor = Color.Black)
+        ) {
+            Text(
+                text = "Дальше",
+                fontSize = 20.sp,
+                color = Color.White
+            )
+        }
     }
 }
